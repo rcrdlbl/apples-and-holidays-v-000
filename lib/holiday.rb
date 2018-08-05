eb8a8428dc839b7bc0d85d1032d14318e888b1a6
Przemyslaw Dabrowski <bluh@outlook.com>
2018-08-05 04:04:40 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -76,6 +76,8 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-  
+  bbq_holidays = []
+  holiday_hash.each do |season, holiday|
+    holiday.each do |holiday, supplies|
 
 end
